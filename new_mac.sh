f49121f6613ba240c47052bbf658649f33bb7f3c
Lincoln Lee <git@lincoln.hk>
2016-06-26 14:17:06 +0800

    Upgrade to ruby 2.3.1

--- new_mac.sh
+++ new_mac.sh
@@ -150,8 +150,8 @@ curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | 
 # install RVM
 \curl -L https://get.rvm.io | bash
 source ~/.rvm/scripts/rvm
-rvm install 2.3.0 --disable-binary
-rvm use --create --default 2.3.0@mac
+rvm install 2.3.1 --disable-binary
+rvm use --create --default 2.3.1@mac
 gem install lunchy
 gem install redcarpet
 rvm @global do gem install rubocop
